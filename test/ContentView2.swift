d839a094b405b2c3ad2ad2a898d510de6cc04b7f
yousraagoub <yousraagoub@gmail.com>
2025-09-29 12:30:50 +0300

    Add Yousra's Presentation.

---
+++ test/ContentView2.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  PresentYourselfChallenge
+//
+//  Created by Yousra Abdelrahman on 07/04/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView2: View {
+    var body: some View {
+        VStack {
+            Image("Image")
+                .resizable()
+                .frame(width: 300, height: 300)
+                .cornerRadius(300)
+                .shadow(radius: 10)
+                .overlay(
+                    Circle().stroke(Color.primary, lineWidth: 5)
+                )
+                .padding(20)
+            
+            VStack (alignment: .leading, spacing: 20) {
+            Text("Yousra Abdelrahman")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(.orange)
+            Text("I am a 25 years-old tech enthusiast who loves learning new things about technology. Also, I believe that there is a lot of beauty and fun in life and I always seek ways to enjoy it more even in the toughest situations.")
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView2()
+}
